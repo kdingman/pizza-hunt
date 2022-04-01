866b3c0adf8de85fae40e8efecf7084469a61570
kdingman <kcdonlan@yahoo.com>
2022-04-01 15:57:05 -0400

    updated server page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,12 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+app.use(require('./routes'));
 
 // Use this to log mongo queries being execute
 mongoose.set('debug', true);
